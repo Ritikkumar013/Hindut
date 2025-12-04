e598a0348fb0e970ae9491ee543d4cca05b73b5f
Ritik Kumar <ritikkashyap013@gmail.com>
2025-12-04 12:51:47 +0530

    FIx api key

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,31 +1,54 @@
-// Import the functions you need from the SDKs
+// // Import the functions you need from the SDKs
+// import { initializeApp } from "firebase/app";
+// // import { getAuth } from "firebase/auth";  
+// import { getAuth, signOut } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore"; // Import Firestore
+// // import { getAnalytics } from "firebase/analytics";
+// // import {
+// //   firebase_api_key,
+// //   authDomain, projectId, storageBucket, messagingSenderId, app_ID, measurementId}
+// // from '../../.env.local';
+
+// // Firebase configuration
+// const firebaseConfig = {
+//   apiKey: process.env.firebase_api_key,
+//   authDomain: process.env.authDomain,
+//   projectId: process.env.projectId,
+//   storageBucket: process.env.storageBucket,
+//   messagingSenderId:  process.env.messagingSenderId,
+//   appId: process.env.app_ID,
+//   measurementId: process.env.measurementId
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// // Initialize Firebase services
+// const auth = getAuth(app);  // Firebase Auth instance
+// const db = getFirestore(app); // Firestore database instance
+
+
+
+// // Export auth and db for use in other parts of your application
+// export { auth, db, signOut };
+
+
 import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";  
 import { getAuth, signOut } from "firebase/auth";
-import { getFirestore } from "firebase/firestore"; // Import Firestore
-// import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
-
-
-// Firebase configuration
 const firebaseConfig = {
-  apiKey: firebase_api_key,
-  authDomain: authDomain,
-  projectId: projectId,
-  storageBucket: storageBucket,
-  messagingSenderId:  messagingSenderId,
-  appId: app_ID,
-  measurementId: measurementId
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-// Initialize Firebase services
-const auth = getAuth(app);  // Firebase Auth instance
-const db = getFirestore(app); // Firestore database instance
-
-
-
-// Export auth and db for use in other parts of your application
 export { auth, db, signOut };
